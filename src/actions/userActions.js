d05e86b1154cca886565e00d2c593c6bfafb53f5
alexkiriungi <mwasxander@gmail.com>
2024-01-21 00:00:52 +0300

    minor changes

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,13 +2,13 @@ const login = (userObj) => {
     return {
         type: 'LOG_IN',
         payload: userObj,
-    }
-}
+    };
+};
 
 const logout = () => {
     return {
         type: 'LOG_OUT',
-    }
-}
+    };
+};
 
 export default { login, logout};
