9f4a9c25d0608378edfbe7458930835f6da33a90
alexkiriungi <mwasxander@gmail.com>
2024-01-20 23:57:13 +0300

    Added the dispatch to useEffects in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import userActions from './actions/userActions';
 
-function App() {
+const App = () => {
+  const currentUser = useSelector((state) => state.currentUser);
+  const dispatch = useDispatch();
+  
+  const user = { name: 'User'};
+
+  useEffect(() => {
+    dispatch(userActions.login(user))
+  });
+
   return (
-    
+    <div className='app'>
+      {currentUser.loggedIn ?
+      (<>
+        <h1>Hi, {currentUser.user.name}</h1>
+        <button onClick={()=> dispatch(userActions.logout())}>logout</button>
+      </>
+      ) : (
+        <>
+          <h1>Login</h1>
+          <button onClick={()=> dispatch(userActions.login(user))}>
+            Logged in as a User
+          </button>
+        </>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
